9d39224f0614680631def1a8340c7ae0d7da7abd
Rishabh Jain <rishabh9843@gmail.com>
2023-12-19 13:09:37 +0530

    Modified registration form to only allow unique username and emails to register

--- coupans_manager/forms.py
+++ coupans_manager/forms.py
@@ -1,6 +1,13 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms import (
+    StringField,
+    PasswordField,
+    SubmitField,
+    BooleanField,
+    ValidationError,
+)
 from wtforms.validators import DataRequired, Length, Email, EqualTo
+from coupans_manager.models import User
 
 
 class RegistrationForm(FlaskForm):
@@ -13,6 +20,16 @@ class RegistrationForm(FlaskForm):
         label="Confirm Password", validators=[DataRequired(), EqualTo("password")]
     )
     submit = SubmitField("Sign Up")
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError("That username is taken.Please choose different one")
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError("That email is taken.Please choose different one")
 
 
 class LoginForm(FlaskForm):
